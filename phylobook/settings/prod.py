52232891dac829bb78fbc0466ca2023b7bfc1a2b
PeterDarley <52110396+PeterDarley@users.noreply.github.com>
2023-02-06 11:01:25 -0800

    Undo some experimental settings changes

--- phylobook/settings/prod.py
+++ phylobook/settings/prod.py
@@ -42,34 +42,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'guardian',
+    'phylobook.uw_saml',
     'treebeard',
 ]
 
 LOGIN_TYPE = os.environ.get('LOGIN_TYPE')
 
-# Don't add UW_Saml if we're not using it, or it's tests will fail
-if LOGIN_TYPE != 'local':
-    INSTALLED_APPS.append('phylobook.uw_saml')
-    # django-guardian backends
-    AUTHENTICATION_BACKENDS = (
-        'phylobook.uw_saml.backends.VVRemoteUserBackend',
-        'django.contrib.auth.backends.ModelBackend', # this is default
-        'guardian.backends.ObjectPermissionBackend',
-    )
-else:
-    # django-guardian backends
-    AUTHENTICATION_BACKENDS = (
-        'django.contrib.auth.backends.ModelBackend', # this is default
-        'guardian.backends.ObjectPermissionBackend',
-    )
-
-
-# django-guardian backends - Moved into above if block
-# AUTHENTICATION_BACKENDS = (
-#     'phylobook.uw_saml.backends.VVRemoteUserBackend',
-#     'django.contrib.auth.backends.ModelBackend', # this is default
-#     'guardian.backends.ObjectPermissionBackend',
-# )
+# django-guardian backends
+AUTHENTICATION_BACKENDS = (
+    'phylobook.uw_saml.backends.VVRemoteUserBackend',
+    'django.contrib.auth.backends.ModelBackend', # this is default
+    'guardian.backends.ObjectPermissionBackend',
+)
 
 # django-guardian - anonymous user object permissions-are disabled.
 ANONYMOUS_USER_NAME = None
