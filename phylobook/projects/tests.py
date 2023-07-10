d9f6f74452388b4a96122bbbfe779e2f0483624b
PeterDarley <52110396+PeterDarley@users.noreply.github.com>
2023-07-10 21:28:46 +0000

    Added ability to parse .svg files

--- phylobook/projects/tests.py
+++ phylobook/projects/tests.py
@@ -2,38 +2,39 @@ from django.test import TestCase
 from django.conf import settings
 
 from phylobook.projects.models import Project, ProjectCategory, Tree, Lineage
+from phylobook.projects.utils import tree_sequences, tree_lineage_counts, parse_sequence_name
 
 # Create your tests here.
 class ProjectTests(TestCase):
     """ Tests that projects can be set up correctly """
 
     def test_create_project(self):
-        """ Create new project and make sure it's name and tree_node are right """
+        """ Create new project and make sure it"s name and tree_node are right """
        
-        my_project = Project(name='My Project')
+        my_project = Project(name="My Project")
         my_project.save()
 
-        self.assertEqual(str(my_project), 'My Project')
-        self.assertEqual(my_project.tree_node(), [{'My Project': {'depth': 1, 'is_project': True}}])
+        self.assertEqual(str(my_project), "My Project")
+        self.assertEqual(my_project.tree_node(), [{"My Project": {"depth": 1, "is_project": True}}])
 
     def test_add_project_to_category(self):
         """ Create a category and add our project to it """
         
-        my_category = ProjectCategory.add_root(name='My Category')
+        my_category = ProjectCategory.add_root(name="My Category")
         my_category.save()
 
         # Create a project with no category then add to category
-        my_project_1 = Project(name='My Project 1') 
+        my_project_1 = Project(name="My Project 1") 
         my_project_1.save()
         my_project_1.category = my_category
 
         # Create a project with a category
-        my_project_2 = Project(name='My Project 2', category = my_category)
+        my_project_2 = Project(name="My Project 2", category = my_category)
         my_project_2.save()
 
         self.assertEqual(my_category.name, my_project_1.category.name)
         self.assertEqual(my_category.name, my_project_2.category.name)
-        # self.assertEqual(my_category.tree_node(), [{'My Category', {'depth': 1, 'is_project': False}}, {'My Project 1', {'depth': 2, 'is_project': True}}, {'My Project 2', {'depth': 2, 'is_project': True}}])
+        # self.assertEqual(my_category.tree_node(), [{"My Category", {"depth": 1, "is_project": False}}, {"My Project 1", {"depth": 2, "is_project": True}}, {"My Project 2", {"depth": 2, "is_project": True}}])
 
 
     def test_migration_should_create_lineages(self):
@@ -46,10 +47,10 @@ class ProjectTests(TestCase):
     def test_should_be_able_to_add_lineage_to_tree(self):
         """ Create a tree and add a lineage to it """
 
-        project = Project(name='My Project 1') 
+        project = Project(name="My Project 1") 
         project.save()
 
-        tree = Tree(name='My Tree', project=project)
+        tree = Tree(name="My Tree", project=project)
         tree.save()
 
         lineage = Lineage.objects.get(color="Red", lineage_name="SxL")
@@ -57,3 +58,115 @@ class ProjectTests(TestCase):
         tree.lineages.add(lineage)
 
         self.assertEqual(tree.lineages.all().first().lineage_name, "SxL")
+
+class TreeTests(TestCase):
+    """ Tests that trees can be parsed """
+
+    # Tests for tree_sequences
+    def test_tree_sequences_should_return_none_given_none(self):
+        """ tree_det_sequence_names should return None when given None """
+
+        self.assertIs(tree_sequences(None), None)
+
+    def test_tree_sequences_should_raise_exception_given_nonexistent_file(self):
+        """ tree_det_sequence_names should raise an exception when given a nonexistent file """
+
+        with self.assertRaises(FileNotFoundError):
+            tree_lineage_counts("bad_name")
+
+    def test_tree_sequences_should_return_list_of_dicts(self):
+        """ tree_det_sequence_names should return a list """
+
+        tree_file_name = "/phylobook/temp/test.svg"
+        sequences = tree_sequences(tree_file_name)
+
+        self.assertIsInstance(sequences, list)
+        self.assertIsInstance(sequences[0], dict)
+
+    def test_tree_sequences_should_not_return_empty(self):
+        """ tree_get_sequences should not return an empty list """
+    
+        tree_file_name = "/phylobook/temp/test.svg"
+        self.assertGreater(len(tree_sequences(tree_file_name)), 0)
+    
+    def test_tree_sequences_should_contain_particular_sequenc(self):
+        """ tree_get_sequences should contain a particular sequence """
+    
+        tree_file_name = "/phylobook/temp/test.svg"
+        sequences = tree_sequences(tree_file_name)
+        my_sequence = [sequence for sequence in sequences if sequence["name"] == "V703_0132_200_GP_NT_70_1"][0]
+
+        self.assertTrue(my_sequence)
+        self.assertEqual(my_sequence["color"], "green")
+
+    # Tests for parse_sequences
+    def test_parse_sequences_should_return_dict(self):
+        """ parse_sequences should return a dict """
+
+        sequence = parse_sequence_name("V703_0132_200_GP_NT_70_1")
+        self.assertIsInstance(sequence, dict)
+
+    def test_parse_sequences_should_not_return_none_when_given_none(self):
+        """ parse_sequences should not return None when given None """
+
+        sequence = parse_sequence_name(None)
+        self.assertIs(sequence, None)
+
+    def test_parse_sequences_should_return_correct_dict(self):
+        """ parse_sequences should return a dict with the correct values """
+
+        sequence = parse_sequence_name("V703_0132_200_GP_NT_70_1")
+        self.assertEqual(sequence, {"timepoint": 200, "multiplicity": 1})
+
+    def test_parse_sequences_should_return_none_with_noncompliant_name(self):
+        """ parse_sequences should return None with a noncompliant name """
+
+        sequence = parse_sequence_name("V703_0132_200_GP_NT_70_1_test")
+        self.assertIs(sequence, None)
+
+        sequence = parse_sequence_name("V703_0132_test_GP_NT_70_1")
+        self.assertIs(sequence, None)
+
+    # Tests for tree_lineage_counts
+    def test_tree_lineage_counts_should_return_dict(self):
+        """ tree_lineage_counts should return a dict """
+
+        tree_file_name = "/phylobook/temp/test.svg"
+        lineage_counts = tree_lineage_counts(tree_file_name)
+
+        self.assertIsInstance(lineage_counts, dict)
+
+    def test_tree_lineage_counts_should_not_return_empty(self):
+        """ tree_lineage_counts should not return an empty list """
+
+        tree_file_name = "/phylobook/temp/test.svg"
+        self.assertGreater(len(tree_lineage_counts(tree_file_name)), 0)
+
+    def test_tree_lineage_counts_should_return_none_given_none(self):
+        """ tree_lineage_counts should return None when given None """
+
+        self.assertIs(tree_lineage_counts(None), None)
+
+    def test_tree_lineage_counts_should_raise_error_given_bad_file_name(self):
+        """ tree_lineage_counts should raise an error when given a bad file name """
+
+        with self.assertRaises(FileNotFoundError):
+            tree_lineage_counts("bad_name")
+
+    def test_tree_lineage_counts_should_return_counts_for_colors(self):
+        """ tree_lineage_counts should return counts for colors """
+
+        tree_file_name = "/phylobook/temp/test.svg"
+        lineage_counts = tree_lineage_counts(tree_file_name)
+
+        self.assertGreater(lineage_counts["red"]["count"], 0)
+    
+    def test_tree_lineage_counts_should_be_less_than_total_count(self):
+        """ tree_lineage_counts should be less than the total count """
+
+        tree_file_name = "/phylobook/temp/test.svg"
+        lineage_counts = tree_lineage_counts(tree_file_name)
+
+        print(lineage_counts["neonblue"]["count"])
+
+        self.assertLess(lineage_counts["red"]["count"], 184)
--- phylobook/projects/utils.py
+++ phylobook/projects/utils.py
@@ -2,7 +2,10 @@ import os, glob
 
 from django.conf import settings
 
+import xml.etree.ElementTree as ET
+
 from phylobook.projects.models import Tree
+
 
 def fasta_type(*, tree: Tree) -> str:
     """ Returns either 'NT' (nucleotide) or 'AA' (amino acid) """
@@ -40,3 +43,71 @@ def fasta_type_by_file_name(*, tree: Tree) -> str:
             return "NT"
 
     return "Unknown"
+
+
+def tree_sequences(tree_file: str) -> list[dict[str: str]]:
+    """ Get all the sequence names from the a specific tree """
+
+    if not tree_file:
+        return None
+
+    root = ET.parse(tree_file).getroot()
+    sequences: list[dict[str: str]] = []
+
+    for sequence in root.iter("{http://www.w3.org/2000/svg}text"):
+        if "class" not in sequence.attrib:
+            continue
+
+        # print(f"\n\n{sequence.text}\n\n")
+        sequences.append({"name": sequence.text, "color": sequence.attrib["class"].replace("box", "")})
+
+    return sequences
+
+
+def parse_sequence_name(sequence_name: str) -> dict:
+    """ Get the timepoint and mulitplicity from a sequence name """
+
+    if not sequence_name:
+        return None
+
+    sequence: dict = {}
+    sequence_bits: list = sequence_name.split("_")
+
+    if sequence_bits[2].isnumeric():
+        sequence["timepoint"] = int(sequence_bits[2])
+    else:
+        return None
+
+    if sequence_bits[-1].isnumeric():    
+        sequence["multiplicity"] = int(sequence_bits[-1])
+    else:
+        return None
+
+    return sequence
+
+
+def tree_lineage_counts(tree_file: str) -> dict[str: dict]:
+    """ Get all the lineage counts from a specific tree """
+
+    if not tree_file:
+        return None
+    
+    sequences = tree_sequences(tree_file)
+    lineage_counts: dict[str: dict] = {}
+
+    for sequence in sequences:
+        attributes = parse_sequence_name(sequence["name"])
+        
+        if not attributes:
+            return None
+        
+        if sequence["color"] not in lineage_counts or attributes["timepoint"] < lineage_counts[sequence["color"]]["timepoint"]:
+            lineage_counts[sequence["color"]] = {
+                "count": attributes["multiplicity"], 
+                "timepoint": attributes["timepoint"]
+            }
+
+        elif attributes["timepoint"] == lineage_counts[sequence["color"]]["timepoint"]:
+            lineage_counts[sequence["color"]]["count"] += attributes["multiplicity"]
+
+    return lineage_counts
